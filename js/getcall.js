8e7234ab44159d7d3da9d1e0e520d04453b438e1
Oliver <oliver.yepez@gmail.com>
2015-06-09 03:39:57 -0600

    Co-Browsing and fixes

--- js/getcall.js
+++ js/getcall.js
@@ -5,7 +5,13 @@ $(function(){
 
     var $apiKey = 'DAK00068abf414f4e6fa818a123a1f3fd4d';
     var $userTo = 'sender@oliveryepez.gmail.com';
-      
+    var $sessionId;
+    var $callerId;
+    var currentSession;
+    
+    var coBrowsinglisteners = {
+    	'onJoinApprove': onSessionJoinApprove
+    }
       // Load audio source to DOM to indicate call events
       var audioSource = {
         ringIn: [
@@ -52,6 +58,7 @@ $(function(){
       	$('#oncall_screen').css('display', 'block');
 
       	setInterval(recieveMessages, 1000);
+      	getOpenSessions();
       }
 
       function onLoginFailed(){
@@ -75,6 +82,7 @@ $(function(){
 
       	$audioRingIn[0].play();
       	callId = call.getId();
+      	callerId = call.callerName();
 
       	if (!isAnonymous) {
       		$('#caller').text(call.callerName);
@@ -86,21 +94,18 @@ $(function(){
        function onCallAnswer(call) {
         callId = call.getId();
 
-        $('#modal_call').modal({
-      			show: 'false'
-      	});
       
         $audioRingOut[0].pause();
         $audioRingIn[0].pause();
+
+        $('#btn_hangup').css('display', 'block');
       }
 
       function onCall(call) {
         console.log('===> oncall');
         $audioRingOut[0].pause();
 
-        $('#modal_call').modal({
-      			show: 'false'
-      	});
+        $('#modal_call').modal('toggle');
        
       }
 
@@ -110,6 +115,8 @@ $(function(){
 
         $audioRingIn[0].pause();
         $audioRingOut[0].pause();
+
+        $('#btn_hangup').css('display', 'none');
       }
 
       function sendMessages(){
@@ -158,6 +165,41 @@ $(function(){
     });
   }
 
+	function onSessionJoinApprove(notification) {
+	  console.log('===> Joined to the session');
+	  KandyAPI.CoBrowse.startBrowsingAgent($sessionId, document.getElementById('cobrowsing-holder'));
+	}
+
+  function getOpenSessions(){
+    kandy.session.getOpenSessions(function(data){
+        //console.log('===> Session ID: ' + data.session_id);
+        var $aSession = data.sessions;
+
+        if($aSession.length > 0){
+          $aSession.forEach(function(session){
+            
+          	//if(session.session_name == 'cobrowsing' && 
+          	 //  session.admin_full_user_id == callerId &&
+          	  // session.session_status == 'active'){
+
+          		console.log('Session Name: ' + session.session_name);
+          		console.log('Session id: ' + session.session_id)
+          		//console.log('Session Admin: ' + session.admin_full_user_id + ' caller: ' + callerId);
+          		console.log('Session status: ' + session.session_status);
+          	//}
+
+            $sessionId = session.session_id;
+             console.log('session id: ' + $sessionId + ' founded!');
+         	});
+         } 
+        
+
+    }, function(data){
+        console.log("Fail to take Open Sessions");
+    });
+  }
+
+
       $('#frmLogin').submit(function(e){
       	e.preventDefault();
       	var $username = $('#txt_username').val();
@@ -183,6 +225,8 @@ $(function(){
       		});
       });
 
+/*************** Call Buttons  ********************/
+
       $('#btn_answer').click(function() {
         kandy.call.answerCall(callId, true);
       });
@@ -194,5 +238,39 @@ $(function(){
       $('#btn_reject').click(function() {
         KandyAPI.Phone.rejectCall(callId);
       });
+/************************************************/
+
+$('#btnRequest').click(function(e){
+	e.preventDefault();	
+	console.log('joining on: ' + $sessionId);
+	kandy.session.setListeners($sessionId, coBrowsinglisteners);
+	kandy.session.join($sessionId, {}, 
+		function(){
+			console.log('===> session join requested: ' + $sessionId);
+			//$('#btnShare').css('display', 'block');
+			//KandyAPI.CoBrowse.startBrowsingAgent(parseInt($sessionId), document.getElementById('cobrowsing-holder'));
+		},
+		function(msg, code){
+			console.log('===> Error joining to session: (' + code + ')' + msg);
+		});
+});
+
+/*$('#btnShare').click(function(e){
+	KandyAPI.CoBrowse.startBrowsingAgent(parseInt($sessionId), document.getElementById('cobrowsing-holder'));
+
+});*/
+
+$('#btnStopCoBrowsing').click(function(e){
+	e.preventDefault();
+
+	/*kandy.session.terminate($sessionId, 
+		function(){
+			console.log('===> session Id: '+ $sessionId + 'terminated');
+		}, 
+		function(){
+			console.log('===> Error ending session!');
+		});*/
+
+});
 
 });
--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ $(function(){
   var password = 'k5p9q6vv4x35504352';
   var $userTo = 'reciever@oliveryepez.gmail.com';
 
+  var $sessionId, $sessionUser;
+
   /*var username = 'costumer'; 
   var password = '12345abc';*/
 
-  var usernameToCall, callId;
+  var callId;
 
    var $audioRingIn = $('<audio>', { loop: 'loop', id: 'ring-in' });
    var $audioRingOut = $('<audio>', { loop: 'loop', id: 'ring-out' });
@@ -35,6 +37,10 @@ $(function(){
         $audioRingOut.append($source);
       });
 
+     var listeners = {
+        'onUserJoinRequest': onSessionUserJoinRequest
+      };
+
 
    kandy.setup({
 
@@ -53,6 +59,41 @@ $(function(){
   function onLoginSuccess(){
     console.log("Login Success!");
     setInterval(recieveMessages, 1000);
+
+    kandy.session.terminate('b3fd5c0d5b91435e942729077204bee8');
+
+     var sessionObject = {
+      session_type: 'cobrowsing',
+      session_name: 'Share Screen'
+    }
+
+   kandy.session.create(sessionObject, function(data){
+      console.log('===> session created ' + data.session_id);
+      $sessionId = data.session_id;
+      
+      kandy.session.getOpenSessionsCreatedByUser(function(list){
+        list.sessions.forEach(function(session){
+          console.log('session admin: ' + session.admin_full_user_id);
+          $sessionUser = session.admin_full_user_id;
+        });
+      }, function(){
+
+      });
+
+
+      kandy.session.activate(data.session_id, 
+        function(){
+          console.log('===> Session id: ' + data.session_id + 'acitvated');
+        },  
+        function(code, msg){
+          console.log('===> error activating session: ' + data_session_id + ' ' + msg + '' + code);
+        });
+
+      kandy.session.setListeners(data.session_id, listeners);
+
+    }, function(msg, code){
+      console.log('===> Error creating session '  + msg + " " + code);
+    });
   }
 
 
@@ -60,8 +101,7 @@ $(function(){
     console.debug('on call!');
     $audioRingOut[0].pause();
 
-    console.log("User = " + usernameToCall);
-
+    console.log("User = " + userTo);
   }
 
   function endcall(){
@@ -153,15 +193,55 @@ $(function(){
     });
   }
 
+  function getOpenSessions(){
+    kandy.session.getOpenSessions(function(data){
+        //console.log('===> Session ID: ' + data.session_id);
+        var $aSession = data.sessions;
+
+        if($aSession.length > 0){
+          $aSession.forEach(function(session){
+           
+
+           if(session.session_type == 'agent_share_screen' && session.admin_full_user_id == $userTo){
+            $sessionId = session.session_id;
+             console.log('session id: ' + $sessionId + ' founded!');
+             kandy.session.terminate($sessionId, function(){
+              console.log('===> session ended!');
+             },
+             function(){
+                console.log('===> error ending session');
+             });
+           }
+
+          });
+        }else{
+          console('===> No Sessions created!');
+        }
+
+    }, function(data){
+        console.log("Fail to take Open Sessions");
+    });
+  }
+
+  function onSessionUserJoinRequest(notification){
+
+    $('#incoming-request').modal({
+      show: 'true'
+    });
+
+    kandy.session.acceptJoinRequest(notification.session_id, notification.full_user_id);
+  }
+
   $('#btnContactUs').on('click', function(){
     $('#modal-call').modal({
       show: 'true'
     });
+
   });
 
   $('#btnMakeCall').on('click', function(){
     
-    kandy.call.makeCall($userTo, false); 
+    kandy.call.makeCall($userTo, false);
 
     $("#btnMakeCall").css('display', 'none');
     $("#btnHangup").css('display', 'block');   
@@ -173,6 +253,7 @@ $(function(){
     $("#btnHangup").css('display', 'none');
 
     kandy.call.endCall(callId);
+    kandy.session.terminate($sessionId);
   })
 
   $('#frm_send').submit(function(e){
@@ -180,9 +261,33 @@ $(function(){
     sendMessages();
   });
  
-   
+  $('#btn_acceptCobrow').click(function(e){
+    e.preventDefault();
+    
+    //kandy.session.acceptJoinRequest($sessionId, $sessionUser);
+      KandyAPI.CoBrowse.startBrowsingUser($sessionId);
+      $("#btnStopShare").css('display','block');
+      $('#incoming-request').modal('toggle');
+    }); 
 
-   kandy.login(APIKey, username, password, onLoginSuccess, onLoginFailed);
+
+   $("#btnStopShare").click(function(){
+      KandyAPI.CoBrowse.stopBrowsingUser();
+      kandy.session.bootUser($sessionId, $userTo, 'stop sharing screen');
+      $("#btnStopShare").css('display','none');
+   });
+
+   $("#btnClose").click(function(){
+      kandy.logout();
+      kandy.session.terminate($sessionId);
+   });
+
+   $("#btnMinimize").click(function(){
+       $('#modal-call').modal('toggle');
+   });
+
+  kandy.login(APIKey, username, password, onLoginSuccess, onLoginFailed);
+
   /* KandyAPI.loginSSO('UAT2d9a0f5333e04d01827c7af6f2f2d8e5',
     function(){
       setup()
